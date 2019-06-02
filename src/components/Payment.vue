cc418395f9c687c65421a02fe7a270f2c6cac2c5
RoxanaJeney <roxana.jeney@web.de>
2019-06-02 17:47:09 +0200

    Month and Expiration Date is now in the same row

--- src/components/Payment.vue
+++ src/components/Payment.vue
@@ -7,7 +7,7 @@
         <img src="@/assets/images/visa.jpg" alt="">
         <img src="@/assets/images/mastercard.jpg" alt="">
     </div>
-    <div class="payment mx-4 mr-5">
+    <div class="payment mx-4 mr-4">
         <form>
             <div class="form-group owner">
                 <label for="owner">Card Holder Name</label>
@@ -21,35 +21,43 @@
                 <label for="cvv">CVV</label>
                 <input type="text" class="form-control" id="cvv">
             </div>
+            <label>Expiration Date</label>
             <div class="form-group" id="expiration-date">
-                <label>Expiration Date</label>
-                <select>
-                    <option value="01">January</option>
-                    <option value="02">February </option>
-                    <option value="03">March</option>
-                    <option value="04">April</option>
-                    <option value="05">May</option>
-                    <option value="06">June</option>
-                    <option value="07">July</option>
-                    <option value="08">August</option>
-                    <option value="09">September</option>
-                    <option value="10">October</option>
-                    <option value="11">November</option>
-                    <option value="12">December</option>
-                </select>
-                <select>
-                    <option value="16"> 2016</option>
-                    <option value="17"> 2017</option>
-                    <option value="18"> 2018</option>
-                    <option value="19"> 2019</option>
-                    <option value="20"> 2020</option>
-                    <option value="21"> 2021</option>
-                    <option value="21"> 2022</option>
-                    <option value="21"> 2023</option>
-                </select>
+                <v-layout>
+                <v-flex xs6>
+                    <select>
+                        <option value="01">January</option>
+                        <option value="02">February </option>
+                        <option value="03">March</option>
+                        <option value="04">April</option>
+                        <option value="05">May</option>
+                        <option value="06">June</option>
+                        <option value="07">July</option>
+                        <option value="08">August</option>
+                        <option value="09">September</option>
+                        <option value="10">October</option>
+                        <option value="11">November</option>
+                        <option value="12">December</option>
+                    </select>
+                </v-flex>
+                <v-flex xs6>
+                    <select>
+                        <option value="16"> 2016</option>
+                        <option value="17"> 2017</option>
+                        <option value="18"> 2018</option>
+                        <option value="19"> 2019</option>
+                        <option value="20"> 2020</option>
+                        <option value="21"> 2021</option>
+                        <option value="21"> 2022</option>
+                        <option value="21"> 2023</option>
+                    </select>
+                </v-flex>
+                </v-layout>
             </div>
             <div class="form-group" id="pay-now">
-                <button type="submit" class="btn btn-default" id="confirm-purchase">Confirm</button>
+                <v-flex class="text-xs-right">
+                    <button type="submit" class="submit" id="confirm-purchase">Confirm</button>
+                </v-flex>
             </div>
         </form>
     </div>
