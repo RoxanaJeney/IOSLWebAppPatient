d5aa24136c905f889a2f22621af7f52fb4e25339
RoxanaJeney <roxana.jeney@web.de>
2019-06-08 23:37:27 +0200

    Minor Changes

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,17 +1,23 @@
 <template>
+<v-layout row wrap align-center justify-center>
+<v-flex xs6 offset xs-1 sm5 offset-sm1 >
+<v-card max-width = "500px" height = "500px">
   <div class="login-wrapper border border-light">
     <form class="form-signin" @submit.prevent="login">
-      <h2 class="form-signin-heading">Please sign in</h2>
-
+      <h2 class="text-md-center" centered>Please sign in</h2>
+      
       <label for="inputUsername" class="sr-only">Username</label>
       <input v-model="name" value="English" type="" id="inputUsername" class="form-control" placeholder="Username">
       
       <label for="inputPassword" class="sr-only">Password</label>
       <input v-model="password" value="" type="password" id="inputPassword" class="form-control" placeholder="Password">
       
-      <button class="btn btn-lg btn-primary btn-block" type="submit" @click="signin">Sign in</button>
+      <button class="btn btn-lg btn-primary btn-block" type="submit" @click="login">Log in</button>
     </form>
   </div>
+</v-card>
+</v-flex>
+</v-layout>
 </template>
 
 <script>
@@ -25,7 +31,7 @@ export default {
     }
   },
   methods: {
-    signin() {
+    login() {
       this.$router.push({ name: "Home", params: { username: this.username }})
     },
   }
