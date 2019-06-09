6c0fc06e2c11d14c3c4384dcd27c61da6d9d8c93
RoxanaJeney <roxana.jeney@web.de>
2019-06-09 13:44:09 +0200

    Minor Changes

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -4,7 +4,7 @@
 <v-card max-width = "500px" height = "500px">
   <div class="login-wrapper border border-light">
     <form class="form-signin" @submit.prevent="login">
-      <h2 class="text-md-center" centered>Please sign in</h2>
+      <h2 class="text-md-center" centered>Please log in</h2>
       
       <label for="inputUsername" class="sr-only">Username</label>
       <input v-model="name" value="English" type="" id="inputUsername" class="form-control" placeholder="Username">
